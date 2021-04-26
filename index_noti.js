91bf4f7696ea186e9bad9953eab395e954997a0e
Arham Saeed <saeedarham100@gmail.com>
2021-04-26 23:39:08 +0500

    Update index_noti.js

--- index_noti.js
+++ index_noti.js
@@ -357,7 +357,7 @@ async function addToCovidPos(db, Timestampo, User, Uuid) {
 var port = process.env.PORT || 3000;
 var app = express();
 //var bodyParser = require("body-parser");
-app.use(express.json());
+app.use('/', express.json());
 // app.use('/user/:id', function (req, res, next) {
 // 	console.log('Request Type:', req.method);
 // 	next();
